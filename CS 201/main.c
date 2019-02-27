bd23492695bdbaf162291130cc58e6e6ce889e5e
TBross <Tbross1@crimson.ua.edu>
2019-02-27 14:55:58 -0600

    Initial Commit

---
+++ CS 201/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CS 201
+//
+//  Created by Trapper Ross on 2/27/19.
+//  Copyright © 2019 Trapper Ross. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
